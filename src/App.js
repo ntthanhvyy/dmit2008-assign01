a61f5e8295c8ef632481b7035da47ca4ea1825d2
Thanh Vy <ntthanhvyy@gmail.com>
2021-02-07 22:59:13 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Stock from './Stock';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Stock></Stock>
+      <form id="f1">
+      <input type="text" ></input>
+      <input type="submit"></input>
+      </form>
+      
+      
+    </div>
+  );
+}
+
+export default App;
